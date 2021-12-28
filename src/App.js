b41b674d27380d82852074279c4a964a4eca015c
ppinner <56121914+ppinner@users.noreply.github.com>
2021-12-28 12:35:48 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import Button from 'react-bootstrap/Button';
+// import {Grid, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<Button>Profile</Button>*/}
+        <h1>
+            Welcome to Take5
+        </h1>
       </header>
+      <footer>
+          {/*<Row className="mx-0">*/}
+              {/*<Button as={Col} variant="primary">Button #1</Button>*/}
+              {/*<Button as={Col} variant="secondary" className="mx-2">Button #2</Button>*/}
+              {/*<Button as={Col} variant="success">Button #3</Button>*/}
+          {/*</Row>*/}
+      </footer>
     </div>
   );
 }
